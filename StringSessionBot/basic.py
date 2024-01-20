da9cab722c5c8369899236951905d0dd91755891
Mrxe11 <127259848+Mrxe11@users.noreply.github.com>
2024-01-20 14:38:20 +0300

    Update basic.py

--- StringSessionBot/basic.py
+++ StringSessionBot/basic.py
@@ -12,33 +12,36 @@ def filter(cmd: str):
 async def start(bot: Client, msg: Message):
     user = await bot.get_me()
     mention = user.mention
+    await asyncio.sleep(1)
+   
     await bot.send_message(
         msg.chat.id,
-        text="** شكرا لاستمرارك معنا**",
-    )
+        text="** شكرا لاستمرارك معنا**"
+    ) 
+  
     await asyncio.sleep(1)
     
     await bot.send_message(
         msg.chat.id,
         text="**كل شيء اصبح بين يديك **",
     )
-    await asyncio.sleep(2)
+    await asyncio.sleep(1)
 
     await bot.send_message(
         msg.chat.id,
         text="**فيزات**",
     )
-    await asyncio.sleep(3)
+    await asyncio.sleep(1)
     await bot.send_message(
         msg.chat.id,
         text="**ايميلات**",
     )
-    await asyncio.sleep(4)
+    await asyncio.sleep(1)
     await bot.send_message(
         msg.chat.id,
         text="**روابط التنصيب لكافة السورسات**",
     )
-    await asyncio.sleep(5)
+    await asyncio.sleep(2)
     await bot.send_message(
         msg.chat.id,
         Data.START.format(msg.from_user.mention, mention),
@@ -50,7 +53,8 @@ async def start(bot: Client, msg: Message):
 @Client.on_message(filter("help"))
 async def _help(bot: Client, msg: Message):
     await bot.send_message(
-        msg.chat.id, Data.HELP, reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+        msg.chat.id, Data.HELP,
+        reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
 
 
@@ -60,17 +64,6 @@ async def about(bot: Client, msg: Message):
     await bot.send_message(
         msg.chat.id,
         Data.ABOUT,
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
-    )
-
-
-# Repo Message
-@Client.on_message(filter("repo"))
-async def repo(bot, msg):
-    await bot.send_message(
-        msg.chat.id,
-        Data.REPO,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons),
     )
